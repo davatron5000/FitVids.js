a6aa41836f47099564f9c45a058a7f603c702c34
Robby Dermody <robbyd@paramise.com>
2012-01-13 13:37:42 -0500

    Fix for more bad HTML (messes things up on the iPhone especially)

--- jquery.fitvids.js
+++ jquery.fitvids.js
@@ -72,7 +72,7 @@
         $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
 
         if (settings.maxWidth && (originalWidth || settings.maxWidthDefault)) {
-          $this.parent('.fluid-width-video-wrapper').wrap('<div class="max-width-video-wrapper" />').parent('.max-width-video-wrapper').css('max-width', originalWidth ? originalWidth+"px" : settings.maxWidthDefault+"px");
+          $this.parent('.fluid-width-video-wrapper').wrap('<div class="max-width-video-wrapper"></div>').parent('.max-width-video-wrapper').css('max-width', originalWidth ? originalWidth+"px" : settings.maxWidthDefault+"px");
         }
 
         $this.removeAttr('height').removeAttr('width');
