595e77f03bf51356d6822e99a2f2af6947248b75
Mohnish Thallavajhula <arrowgunz@gmail.com>
2012-05-09 13:07:53 -0700

    This adds support to both https and http versions of YouTube, Vimeo and Kickstarter

--- jquery.fitvids.js
+++ jquery.fitvids.js
@@ -46,10 +46,9 @@
     
     return this.each(function(){
       var selectors = [
-        "iframe[src^='http://player.vimeo.com']", 
-        "iframe[src^='http://www.youtube.com']", 
-        "iframe[src^='https://www.youtube.com']", 
-        "iframe[src^='http://www.kickstarter.com']", 
+        "iframe[src*='player.vimeo.com']", 
+        "iframe[src*='www.youtube.com']",  
+        "iframe[src*='www.kickstarter.com']", 
         "object", 
         "embed"
       ];
