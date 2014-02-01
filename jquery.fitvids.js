eea7a2ad5371a54e4be6c2c89db52717160ea569
Michael Lindahl <lindahl22@gmail.com>
2014-02-01 12:19:36 -0800

    Aborted fluid width for media with a width smaller than 320px

--- jquery.fitvids.js
+++ jquery.fitvids.js
@@ -57,6 +57,7 @@
 
       $allVideos.each(function(){
         var $this = $(this);
+        if ($this.width() < 320) return false;
         if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
         var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
             width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
